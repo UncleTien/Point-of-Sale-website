cd7364ae7bff048835f04a2ae5b284eaad84565c
Goldypham21 <132685186+Goldypham21@users.noreply.github.com>
2023-12-17 15:28:11 +0700

    Add files via upload

---
+++ Point-of-Sale-website/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Point_of_Sale_website.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; } // Thêm trường Description
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; } // Khóa ngoại đến danh mục
+    }
+
+}
